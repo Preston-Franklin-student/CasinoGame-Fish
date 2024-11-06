94323eeb66c337d94d4d88468312f1716dc31eef
HelloJacobW <jawoa4d@student.oaisd.org>
2024-11-06 19:13:45 +0000

    Added HorceRace Method

--- obj/Debug/net8.0/CasinoGame-Fish.AssemblyInfo.cs
+++ obj/Debug/net8.0/CasinoGame-Fish.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CasinoGame-Fish")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3abe92591d15f63ec543464eca29f797feceeb26")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e65a88a11319ae46c359ca2d590cc0fe306ec503")]
 [assembly: System.Reflection.AssemblyProductAttribute("CasinoGame-Fish")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CasinoGame-Fish")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
