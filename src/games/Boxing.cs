408b86d367b79d2de0ba08438ea22853cb439d74
Dylan Bruner <brunerdylan5@gmail.com>
2024-11-08 02:06:13 -0500

    cleanup the boxing code a bit

--- src/games/Boxing.cs
+++ src/games/Boxing.cs
@@ -7,19 +7,15 @@ public class Boxing : Game
         Console.Clear();
         Random random = new Random();
         List<int> critNums = new List<int> { 0, 0, 0, 1, 1, 1, 1, 1, 1, 2 };
-        string p1 = Constants.nameList[random.Next(Constants.nameList.Count)];
-        string p2 = Constants.nameList[random.Next(Constants.nameList.Count)];
+        
+        Player p1 = new();
+        Player p2 = new();
 
-        Console.WriteLine($"{p1} and {p2} are boxing.");
+        Console.WriteLine($"{p1.name} and {p2.name} are boxing.");
         Console.WriteLine($"You have ${gameState.money}.");
 
-        // Player 1 stats initialization
-        int p1Defense = random.Next(0, 5), p1Health = random.Next(15, 21), p1Speed = random.Next(1, 11), p1Damage = random.Next(5, 11);
-        DisplayPlayerStats(p1, p1Damage, p1Defense, p1Health, p1Speed);
-
-        // Player 2 stats initialization
-        int p2Defense = random.Next(0, 5), p2Health = random.Next(15, 21), p2Speed = random.Next(1, 11), p2Damage = random.Next(5, 11);
-        DisplayPlayerStats(p2, p2Damage, p2Defense, p2Health, p2Speed);
+        DisplayPlayerStats(p1);
+        DisplayPlayerStats(p2);
 
 
         // Betting and game logic
@@ -48,87 +44,87 @@ public class Boxing : Game
         {
             Console.Write("On who? ");
             guess = Console.ReadLine().ToLower();
-            if (guess == p1.ToLower() || guess == p2.ToLower())
+            if (guess == p1.name.ToLower() || guess == p2.name.ToLower())
             {
                 break;
             }
             Console.WriteLine("You can't be indifferent!");
         }
         Console.Clear();
-        while (p1Health > 0 && p2Health > 0)
+        while (p1.health > 0 && p2.health > 0)
         {
-            Console.WriteLine($"    Health: {p1Health}");
+            Console.WriteLine($"    Health: {p1.health}");
             Console.WriteLine("|--------------|");
             Console.Write("|");
-            for (int i = 0; i < 14 - p1.Length; i++)
+            for (int i = 0; i < 14 - p1.name.Length; i++)
             {
                 Console.Write(" ");
-                if (i == (14 - p1.Length - 1) / 2)
+                if (i == (14 - p1.name.Length - 1) / 2)
                     Console.Write(p1);
             }
             Console.WriteLine("|");
             Console.WriteLine("|              |");
             Console.WriteLine("|              |");
             Console.Write("|");
-            for (int i = 0; i < 14 - p2.Length; i++)
+            for (int i = 0; i < 14 - p2.name.Length; i++)
             {
                 Console.Write(" ");
-                if (i == (14 - p2.Length - 1) / 2)
+                if (i == (14 - p2.name.Length - 1) / 2)
                     Console.Write(p2);
             }
             Console.WriteLine("|");
             Console.WriteLine("|--------------|");
-            Console.WriteLine($"    Health: {p2Health}");
+            Console.WriteLine($"    Health: {p2.health}");
             Console.Write("Press enter to continue.");
             Console.ReadLine();
             Console.Clear();
-            if (p1Speed > p2Speed)
+            if (p1.speed > p2.speed)
             {
                 // p1 attacks first
                 int crit = critNums[random.Next(critNums.Count)];
-                int damageDealt = crit * (p1Damage - p2Defense);
+                int damageDealt = crit * (p1.damage - p2.defense);
                 Console.WriteLine($"{p1} does {damageDealt} damage to {p2}!");
-                p2Health -= damageDealt;
+                p2.health -= damageDealt;
 
-                if (p2Health > 0)
+                if (p2.health > 0)
                 {
                     crit = critNums[random.Next(critNums.Count)];
-                    damageDealt = crit * (p2Damage - p1Defense);
+                    damageDealt = crit * (p2.damage - p1.defense);
                     Console.WriteLine($"{p2} does {damageDealt} damage to {p1}!");
-                    p1Health -= damageDealt;
+                    p1.health -= damageDealt;
                 }
             }
-            else if (p2Speed > p1Speed)
+            else if (p2.speed > p1.speed)
             {
                 // p2 attacks first
                 int crit = critNums[random.Next(critNums.Count)];
-                int damageDealt = crit * (p2Damage - p1Defense);
+                int damageDealt = crit * (p2.damage - p1.defense);
                 Console.WriteLine($"{p2} does {damageDealt} damage to {p1}!");
-                p1Health -= damageDealt;
+                p1.health -= damageDealt;
 
-                if (p1Health > 0)
+                if (p1.health > 0)
                 {
                     crit = critNums[random.Next(critNums.Count)];
-                    damageDealt = crit * (p1Damage - p2Defense);
+                    damageDealt = crit * (p1.damage - p2.defense);
                     Console.WriteLine($"{p1} does {damageDealt} damage to {p2}!");
-                    p2Health -= damageDealt;
+                    p2.health -= damageDealt;
                 }
             }
             else
             {
                 int crit = critNums[random.Next(critNums.Count)];
-                int damageDealt = crit * (p1Damage - p2Defense);
+                int damageDealt = crit * (p1.damage - p2.defense);
                 Console.WriteLine($"{p1} does {damageDealt} damage to {p2}!");
-                p2Health -= damageDealt;
+                p2.health -= damageDealt;
                 crit = critNums[random.Next(critNums.Count)];
-                damageDealt = crit * (p2Damage - p1Defense);
+                damageDealt = crit * (p2.damage - p1.defense);
                 Console.WriteLine($"{p2} does {damageDealt} damage to {p1}!");
-                p1Health -= damageDealt;
+                p1.health -= damageDealt;
             }
 
         }
         Console.WriteLine("Game!");
-        if ((guess == p1.ToLower() && p2Health <= 0) || (guess == p2.ToLower() && p1Health <= 0))
+        if ((guess == p1.name.ToLower() && p2.health <= 0) || (guess == p2.name.ToLower() && p1.health <= 0))
         {
             gameState.money += bet;
             Console.WriteLine($"You were right! You win ${bet}!\nYou now have ${gameState.money}.");
@@ -143,9 +139,9 @@ public class Boxing : Game
         Console.ReadLine();
     }
 
-    private void DisplayPlayerStats(string player, int damage, int defense, int health, int speed)
+    private void DisplayPlayerStats(Player p)
     {
-        string[] stats = { damage.ToString(), defense.ToString(), health.ToString(), speed.ToString() };
+        string[] stats = { p.damage.ToString(), p.defense.ToString(), p.health.ToString(), p.speed.ToString() };
 
         if (gameState.loseSwitch)
         {
@@ -161,10 +157,21 @@ public class Boxing : Game
         }
 
 
-        Console.WriteLine($"{player}'s stats:");
+        Console.WriteLine($"{p.name}'s stats:");
         Console.WriteLine($"Attack: {stats[0]}");
         Console.WriteLine($"Defense: {stats[1]}");
         Console.WriteLine($"Health: {stats[2]}");
         Console.WriteLine($"Speed: {stats[3]}");
+    }
+
+    private class Player {
+        private static Random rnd = new();
+
+        public readonly string name = Constants.nameList[rnd.Next(Constants.nameList.Count)];
+        public readonly int defense = rnd.Next(0, 5);
+        public readonly int speed = rnd.Next(1, 11);
+        public readonly int damage = rnd.Next(5, 11);
+
+        public int health = rnd.Next(15, 21);
     }
 }
