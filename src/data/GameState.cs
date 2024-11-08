d684a391ee9d0c74b931e68a3dc181e2f9ee61ea
Dylan Bruner <brunerdylan5@gmail.com>
2024-11-08 02:37:11 -0500

    stop the drunk level from ever going negative too

--- src/data/GameState.cs
+++ src/data/GameState.cs
@@ -2,11 +2,12 @@ public class GameState {
     public readonly SaveData saveData = new SaveData("data/savedata.props");
     public bool loseSwitch = false;
     public int drunkLevel {
-        get { return saveData.GetOrDefault("drunk-level", 0); }
-        set { saveData.SetValue("drunk-level", value); }
+        get => saveData.GetOrDefault("drunk-level", 0);
+        set { saveData.SetValue("drunk-level", Math.Max(0, value)); }
     }
-    public int money {
-        get { return saveData.GetOrDefault<int>("money", 200); }
+    public int money
+    {
+        get => saveData.GetOrDefault("money", 200);
         set { saveData.SetValue("money", Math.Max(0, value)); }
     }
 }
