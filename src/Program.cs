1fc6ad89931d22557d504d4a67c9b09d9c91e9e3
Dylan Bruner <brunerdylan5@gmail.com>
2024-11-08 01:09:05 -0500

    move the bar into it's own 'Game'

--- src/Program.cs
+++ src/Program.cs
@@ -99,7 +99,7 @@ class Program
                     Credits();
                     break;
                 case "bar":
-                    gameState.drunkLevel += Bar();
+                    new Bar().PlayGame(gameState);
                     break;
                 case "horses":
                     new HorseRace().PlayGame(gameState);
@@ -122,55 +122,6 @@ class Program
         Thread.Sleep(7000);
         string words = "And Ur Mom!";
         Helpers.Typing(gameState, words);
-    }
-
-    static int Bar()
-    {
-        string UserAnswer;
-        Console.Clear();
-        if (gameState.drunkLevel <= 3)
-        {
-            Console.Write("You are at the bar.\nDo you want a drink for $10? (y/n): ");
-            UserAnswer = Console.ReadLine();
-        }
-        else
-        {
-            Console.WriteLine("You go back to the bar and order another dwink.");
-            UserAnswer = "y";
-        }
-        if (UserAnswer == "y")
-        {
-            gameState.money -= 10;
-            Random rand = new Random();
-            int RandNum = rand.Next(1, 4);
-            Thread.Sleep(1000);
-            Console.Write("You take a drink.");
-            for (int i = 0; i < 4; i++)
-            {
-                Thread.Sleep(1000);
-                Console.Write(".");
-            }
-            Console.WriteLine();
-            Thread.Sleep(1000);
-            if (RandNum == 1)
-            {
-                Console.WriteLine("You feel... drunk...");
-                Thread.Sleep(4000);
-                return 2;
-            }
-            else
-            {
-                Console.WriteLine("Nothing happens.");
-                Thread.Sleep(4000);
-                return 1;
-            }
-        }
-        else
-        {
-            Console.WriteLine("You go to the bar yet don't even sit down.\nYou don't know what you are doing.");
-            Thread.Sleep(4000);
-            return 0;
-        }
     }
 
     static void Credits()
---
+++ src/games/Bar.cs
@@ -0,0 +1,52 @@
+public class Bar : Game
+{
+    public override string Name => throw new NotImplementedException();
+
+    public override void PlayGame(GameState gameState)
+    {
+        string UserAnswer;
+        Console.Clear();
+        if (gameState.drunkLevel <= 3)
+        {
+            Console.Write("You are at the bar.\nDo you want a drink for $10? (y/n): ");
+            UserAnswer = Console.ReadLine();
+        }
+        else
+        {
+            Console.WriteLine("You go back to the bar and order another dwink.");
+            UserAnswer = "y";
+        }
+        if (UserAnswer == "y")
+        {
+            gameState.money -= 10;
+            Random rand = new Random();
+            int RandNum = rand.Next(1, 4);
+            Thread.Sleep(1000);
+            Console.Write("You take a drink.");
+            for (int i = 0; i < 4; i++)
+            {
+                Thread.Sleep(1000);
+                Console.Write(".");
+            }
+            Console.WriteLine();
+            Thread.Sleep(1000);
+            if (RandNum == 1)
+            {
+                Console.WriteLine("You feel... drunk...");
+                Thread.Sleep(4000);
+                gameState.drunkLevel += 2;
+            }
+            else
+            {
+                Console.WriteLine("Nothing happens.");
+                Thread.Sleep(4000);
+                gameState.drunkLevel += 1;
+            }
+        }
+        else
+        {
+            Console.WriteLine("You go to the bar yet don't even sit down.\nYou don't know what you are doing.");
+            Thread.Sleep(4000);
+        }
+    }
+}
