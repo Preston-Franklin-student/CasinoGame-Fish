8dd854ce6241bbbd1eedb5498e3cdcfab589d47b
Dylan Bruner <brunerdylan5@gmail.com>
2024-11-08 02:40:15 -0500

    cleanup the code a bit, replace some code with the new methods

--- src/games/CoinFlip.cs
+++ src/games/CoinFlip.cs
@@ -5,76 +5,50 @@ public class CoinFlip : Game
     public override void PlayGame()
     {
         Console.Clear();
-        Random jimmy = new Random();
-        int amount = (int)(.30 * gameState.money);
+        Random rnd = new Random();
+        int amount = (int)(gameState.money * 0.30);
         int multiply = 2;
         if (gameState.loseSwitch)
             multiply++;
-        string name = Constants.nameList[jimmy.Next(Constants.nameList.Count)];
+
+        string name = Constants.nameList[rnd.Next(Constants.nameList.Count)];
         Console.WriteLine($"{name} challenges you to a coin flip!\nThey bet you ${amount}!");
-        while (true)
-        {
-            if (Helpers.AskYesNo("Do you accept? (yes or no): "))
-            {
-                Console.Write("Heads or Tails? (h or t): ");
-                char picked = char.Parse(Console.ReadLine());
-                while (true)
-                {
-                    if (picked != 'h' && picked != 't')
-                    {
-                        Console.WriteLine("That's not heads or tails!");
-                        Console.Write("Heads or Tails? (h or t): ");
-                        picked = char.Parse(Console.ReadLine());
-                        continue;
-                    }
-                    else
-                    {
-                        break;
-                    }
-                }
-                string letters = "ht";
-                Console.WriteLine("Flipping Coin!!");
-                for (int i = 0; i < 3; i++)
-                {
-                    Console.Write(".");
-                    Thread.Sleep(1000);
-                }
-                Console.WriteLine("");
-                int coinresult = jimmy.Next(0, 2);
-                if (letters[coinresult] == picked)
-                {
-                    if (picked == 'h')
-                    {
-                        Console.WriteLine($"You were right it was heads!\nYou win ${amount}!");
-                    }
-                    else
-                    {
-                        Console.WriteLine($"You were right it was tails!\nYou win ${amount}!");
-                    }
-                    gameState.money += amount;
-                }
-                else
-                {
-                    if (picked == 't')
-                    {
-                        Console.WriteLine($"You were wrong it was heads!\nYou lose ${amount}!");
-                    }
-                    else
-                    {
-                        Console.WriteLine($"You were wrong it was tails!\nYou lose ${amount}!");
-                    }
-                    gameState.money -= amount;
-                }
-                Thread.Sleep(5000);
-                break;
-            }
-            else
-            {
-                Console.WriteLine($"{name} doesn't like that! You robs you of ${amount * multiply}!");
-                gameState.money -= amount * multiply;
-                Thread.Sleep(5000);
-                break;
-            }
+        
+        while (!Helpers.AskYesNo("Do you accept? (yes or no): ")) {
+            Console.WriteLine($"{name} doesn't like that! {name} robs you of ${amount * multiply}!");
+            gameState.money -= amount * multiply;
+            Helpers.SkippableDelay(5000);
         }
+
+        Console.Write("Heads or Tails? (h or t): ");
+        char picked = char.Parse(Console.ReadLine()!);
+        while (picked != 'h' && picked != 't')
+        {
+            Console.WriteLine("That's not heads or tails!");
+            Console.Write("Heads or Tails? (h or t): ");
+            picked = char.Parse(Console.ReadLine()!);
+        }
+
+        Console.WriteLine("Flipping Coin!!");
+        for (int i = 0; i < 3; i++)
+        {
+            Console.Write(".");
+            Thread.Sleep(1000);
+        }
+
+        Console.WriteLine();
+        char result = rnd.Next(0, 2) == 1 ? 'h' : 't';
+
+        if (result == picked)
+        {
+            Console.WriteLine($"You were right it was {result}!\nYou win ${amount}!");
+            gameState.money += amount;
+        }
+        else
+        {
+            Console.WriteLine($"You were wrong it was {result}!\nYou lose ${amount}!");
+            gameState.money -= amount;
+        }
+        Helpers.SkippableDelay(5000);
     }
 }
