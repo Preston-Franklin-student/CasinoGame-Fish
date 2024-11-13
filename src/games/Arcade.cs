64df23c6e06c7aa46abc0d087e003069c3fa74a7
Preston-Franklin-student <prfr981@student.oaisd.org>
2024-11-13 19:38:02 +0000

    Started an arcade

---
+++ src/games/Arcade.cs
@@ -0,0 +1,14 @@
+public class Arcade : Game
+{
+    public override string Name => "Arcade";
+
+    public override void Play()
+    {
+        Console.WriteLine("Welcome to the arcade!");
+        Console.WriteLine("What game do you want to play!");
+
+    }
+    static void SimonSays(){
+        int[] array1 = new 
+    }
+}
